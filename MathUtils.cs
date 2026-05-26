7dca6cc8f5fe04ca9d66f5c5f8b654ed88c35fe5
Павел Чернышев <pavel.chernyshev08@mail.ru>
2026-05-26 10:48:38 +0300

    Добавлен класс MathUtils.

---
+++ MathUtils.cs
@@ -0,0 +1,16 @@
+namespace CSharpLab
+{
+	public class Helper
+	{
+		/// <summary>
+		/// Метод для сложения двух чисел.
+		/// </summary>
+		/// <param name="a">Первое число.</param>
+		/// <param name="b">Второе число.</param>
+		/// <returns>Сумма чисел a и b.</returns>
+		public static int AddNumbers(int a, int b)
+		{
+			return a + b;
+		}
+	}
+}
